c3934aa1f3fc905c84ae677927bbef0a46fda793
Suckovich <iwill@nottellyou.myemail>
2017-06-14 14:34:14 +0300

    Goodized
    Прибрал кучу г-на в одну функцию, добавил время в логирование.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import vk_api
 from time import sleep
-
+from datetime import datetime
 vinchik = -91050183
 
 authlist = {'login': [], 'password': []}
@@ -22,38 +22,22 @@ for a in range (0, len(authlist['login'])):
     vk_session.append(vk_api.VkApi(authlist['login'][a], authlist['password'][a]))
     vk_session[a].auth()
 
-    def core (vk):
-        message = vk.messages.getHistory(count = 1, user_id = vinchik)
-        
-        if message['items'][0]['body'].find(u'Нашел кое-кого для тебя, смотри:') != -1:
-            vk.messages.send(user_id = vinchik, message = '1')
-            print ('gotcha working')
-            sleep(2)
-            
-        if message['items'][0]['body'].find(u'1. Оценить еще кого-то.') != -1:
-            vk.messages.send(user_id = vinchik, message = '1')
-            print ('gotcha pair')
-            sleep(2)
-            
-        if message['items'][0]['body'].find(u'Кое-кому понравилось твое предложение') != -1:
-            vk.messages.send(user_id = vinchik, message = '1')
-            print ('gotcha response')
-            sleep(2)
-        if message['items'][0]['body'].find(u'Есть новости по твоей анкете') != -1:
-            vk.messages.send(user_id = vinchik, message = '1')
-            print ('gotcha smth')
-            sleep(2)
-            
-        if message['items'][0]['body'].find(u'Ждем ответа от этого пользователя') != -1:
-            vk.messages.send(user_id = vinchik, message = '1')
-            print ('gotcha smth2')
-            sleep(2)
-            
-        if message['items'][0]['body'].find(u'Кто-то тобой заинтересовался!') != -1:
-            vk.messages.send(user_id = vinchik, message = '1')
-            print ('gotcha interest')
-            sleep(2)
-        
+def response (amessage, rmessage, resp, log):
+    if amessage['items'][0]['body'].find(rmessage) != -1:
+        vk.messages.send(user_id = vinchik, message = resp)
+        d = datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S")
+        print (d + ' ' + log)
+
+def core (vk):
+    message = vk.messages.getHistory(count = 1, user_id = vinchik)
+       
+    response(message, u'Нашел кое-кого для тебя, смотри:', '1', 'ans 1')
+    response(message, u'1. Оценить еще кого-то.', '1', 'ans 2')
+    response(message, u'Кое-кому понравилось твое предложение', '1', 'ans 3')
+    response(message, u'Есть новости по твоей анкете', '1', 'ans 4')
+    response(message, u'Ждем ответа от этого пользователя', '1', 'ans 5')
+    response(message, u'Кто-то тобой заинтересовался!', '1', 'ans 6')
+    
 while (228==228):
     for api in vk_session:
         vk = api.get_api()
